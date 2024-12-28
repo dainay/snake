088b4692deb8a7a54896b6d07e9d3eb22e4ad91f
dainay <115573080+dainay@users.noreply.github.com>
2024-12-28 12:26:20 +0100

    win song

--- src/components/win/Win.jsx
+++ src/components/win/Win.jsx
@@ -1,6 +1,28 @@
 import s from "./Win.module.scss";
+import { useEffect, useRef } from "react";
 
 const Win = ({ replay }) => {
+  const endAudioRef = useRef(null);
+
+  useEffect(() => {
+    // Play the end audio
+    const endAudio = new Audio("audio/end.mp3");
+    endAudio.play();
+    endAudioRef.current = endAudio; // Save the audio instance to the ref
+  }, []);
+
+  const handleReplay = () => {
+    // Stop the end audio
+    if (endAudioRef.current) {
+      endAudioRef.current.pause();
+      endAudioRef.current.currentTime = 0;
+    }
+
+    // Call the replay function
+    replay();
+  };
+
+
   return (
     <div className={s.win}>
       <div className={s.wrapper}>
@@ -20,7 +42,7 @@ const Win = ({ replay }) => {
               "Why worry about getting in trouble when it’s so much fun to cause?" —
               George Weasley
             </blockquote>
-            <button onClick={replay}>We need more DA members!</button>
+            <button onClick={handleReplay}>We need more DA members!</button>
             <a className={s.link} href="https://dariaiarovaia.great-site.net/">Who are you?</a>
                   </div>
         </div>
